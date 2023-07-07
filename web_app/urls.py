dbaf2502042cf24e3c654ea16e86b3d9355d8f51
Jeph2001 <josephmaniragaba9@gmail.com>
2023-07-07 10:54:54 +0200

    solving some urls conflicts

--- web_app/urls.py
+++ web_app/urls.py
@@ -3,11 +3,12 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.home),
     path('app/', views.landing),
     path('admindata/', views.get_board),
     path('board/', views.topic_data),
     path('post/', views.post_data),
     path('create/', views.get_topic, name='get_topic'),
     path('confirm/', views.confirming),
-    path('chat/', views.get_post),
+    path('chatting/', views.get_post, name='get_post'),
 ]
