736b6c0f2a90df1841b1f96d0bce1c845ee1a965
Jeph2001 <josephmaniragaba9@gmail.com>
2023-07-11 10:37:01 +0200

    adding the logout option on my app

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('', views.login),
+    path('logout/', auth_views.LogoutView.as_view()),
     path('signup/', views.sign_up),
     path('confirm/', views.confirming),
     path('reset/', views.password_reset),
